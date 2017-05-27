89f31284586ccd3ba6e8e72daa94f5f5b9104020
EbisuDev <ebisu.mobile@gmail.com>
2017-05-27 13:46:41 +0200

    Lab8 first commit

--- test/app.spec.js
+++ test/app.spec.js
@@ -3,17 +3,20 @@
 var expect = require('chai').expect;
 var app = require('../app/app');
 
-describe('reverseNumber', function ()
-{
-
+describe('reverseNumber', function () {
+    it('should return number with reversed digits', function () {
+        expect(app.reverseNumber(135)).to.eql(531);
+    });
 });
 
-describe('returnOnlyLetter', function ()
-{
-
+describe('returnOnlyLetter', function () {
+    it('should return string with non-letters removed', function () {
+        expect(app.returnOnlyLetter('a1b2c3')).to.eql('abc');
+    });
 });
 
-describe('isEmail', function ()
-{
-
+describe('isEmail', function () {
+    it('should return if string matches email pattern', function () {
+        expect(app.isEmail("hello@world.com")).to.eql(true);
+    })
 });
